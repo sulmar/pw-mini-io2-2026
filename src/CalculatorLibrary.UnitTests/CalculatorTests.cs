bf58de6a51a81d5eeec72ab8c7ebb44f3521083f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-02-23 11:05:21 +0100

    Added Calculator with UnitTests

---
+++ src/CalculatorLibrary.UnitTests/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿namespace CalculatorLibrary.UnitTests;
+
+public class CalculatorTests
+{
+    private Calculator sut; // System Under Testing
+    
+    public CalculatorTests()
+    {
+        // Arrange
+        sut = new Calculator();
+    }
+    
+    // {Method}_{Scenario}_{ExcectedBehavior}
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(2, 2, 4)]
+    public void Add_PositiveNumbers_ReturnsSummary(int x, int y, int expected)
+    {
+        // Act
+        var result = sut.Add(x, y);
+        
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void Add_NegativeNumbers_ThrowsException()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentException>(()=>sut.Add(-1, 0));
+    }
+    
+}
---
+++ src/CalculatorLibrary/Calculator.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculatorLibrary;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        NumberValidator.Validate(a, b);
+
+        return a+b;
+    }
+
+ 
+}
+
+
+public class NumberValidator
+{
+    public static void Validate(int a, int b)
+    {
+        if (a < 0 || b < 0)
+            throw new ArgumentException();
+    }
+}
